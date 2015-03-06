ef01fc4351ae926c963f9d9a04843f3b091b9a54
Ilmi <ilmiramadani90@gmail.com>
2015-03-06 12:45:19 +0100

    arrays + loops
    array and for loops examples

---
+++ c10.php
@@ -0,0 +1,98 @@
+<?php
+
+// for(/*start*/; /*kraj*/; chekor) {
+
+// 	// kod 
+// }
+
+
+
+// Te gjithat jane te njejta per cdonjeren do kete ritje me 1; (zgolemuvanje za 1)
+// $i = $i + 1;
+// $i += 1;
+// $i++;
+
+
+// for ($i=0; $i < 10; $i++) {
+
+// 	echo $i;
+// 	echo"<br>";
+
+// } 
+
+
+
+
+// FOR LOOP EXAMPLES !!!
+
+// for ($i=10; $i < 100; $i += 2) {
+
+// 	echo $i;
+// 	echo "<br>";
+// }
+
+
+// echo "<br>";
+
+// for ($i=-8; $i < 16; $i += 3) {
+
+// 	echo $i;
+// 	echo "<br>";
+// }
+
+// echo '<br>';
+
+// for ($i=20; $i < 22; $i++) {
+
+// 	echo $i;
+// 	echo "<br>";
+// }
+
+// echo '<br>';
+
+// for ($i=90; $i < 111; $i+=8) {
+
+// 	echo $i;
+// 	echo "<br>";
+// }
+
+// echo '<br>';
+
+// for ($i=0; $i > -11; $i-=2) {
+
+// 	echo $i;
+// 	echo "<br>";
+// }
+
+
+// FOR LOOP EXAMPLES !!!
+
+echo "<br>";
+
+for ($i=0; $i < 30; $i++) {
+
+	if($i % 2 == 0) {
+
+	echo $i;
+	echo "<br>";
+		
+	}
+
+}
+
+echo "<br>";
+
+$broj = 0;
+
+for ($i = 10; $i < 50; $i++) {
+
+	$broj = $broj + $i;
+	 // $broj += $i;
+	
+	}
+	
+	echo $broj;
+
+
+?>
+
---
+++ c9.php
@@ -0,0 +1,178 @@
+<?php
+
+
+// $a = 1;
+// echo $a;
+
+// $a = array (3, 5, 1, 8 , 11,'pero');
+// echo $a;
+
+// print_r($a); 
+// Vetem per testim, asnjeher mos e perdor ne web!
+
+// echo $a[3]; Definimi i array.
+
+
+
+
+// $a = array ();
+
+// $a[] = 'pero';
+// $a[] = 'janko';
+// $a[] = 'stanko';
+
+// // Shkrimi i array pa mos i caktuar ne numer rendor.
+// print_r($a);
+
+
+
+
+
+// $a[5] = 'Skopje';
+// $a[11] = 'Tetovo';
+// $a[101] = 'Gostivar';
+// $a[6] = 'Gevgelija';
+// $a[] = 'Kumanovo';
+
+// print_r($a);
+
+// ne kete menyre i japim indekset vete si duam ne shembull: [5], [101], [6]
+
+
+
+
+
+// $a = array(10 => 'Makedonija', 20 => 'Germanija', 50 => 'Zimbabwe');
+
+// print_r($a);
+
+// ne kete menyre i caktojme vetem indekset dhe u japim vlere.
+
+
+
+
+// $a[2] = "Hamburger";
+// $a[10] = "Tost";
+// $a[4] = "Burek";
+// $a[32] = "Kebap";
+// $a[67] = "Salata";
+// $a[5] = "Supa";
+// $a[9] = "Pizza";
+// $a[89] = "Kompir";
+// $a[55] = "Leb";
+// $a[] = "Gumbyr";
+
+// print_r($a);
+
+//Shembull 1
+
+
+
+
+
+
+// $a = array (
+// 	TRUE,
+// 	'String', 
+// 	1001, 
+// 	3.14,
+	
+// 	array (
+
+// 		'pero',
+// 		'janko',
+// 		'stanko'
+// 		),
+
+// 	array (
+
+// 		'skopje',
+// 		'paris',
+// 		'london'
+// 		)
+// 	);
+// // print_r($a);
+
+// echo $a[4][1];
+// echo '<br>';
+// echo $a[5][2];
+// echo '<br>';
+// echo $a[4][2];
+
+// array ne array, caktimi i disa array ne array dhe targetimi i tyre
+// shembull echo $a [4] [1]; do te targetoj Janko. Keto jane array me Indekse.
+
+
+
+// $student = array ();
+
+// $student['ime'] = 'Bojan';
+// $student ['prezime'] = 'Gavrovski';
+// $student ['telefon'] = '223005';
+// $student ['adresa'] = 'Ilindenska 203';
+// $student ['email'] ='boki007@gmail.com';
+// $student ['kolla'] ='mercedes';
+// $student ['grad'] = 'Skopje';
+
+
+// print_r($student);
+
+// echo '<br>';
+
+
+
+
+
+// $tabela = array (
+
+
+// array(
+
+// 	'ime'=>'Bojan',
+// 	'prezime'=>'Gavrovski',
+// 	'telefon'=>'223305',
+
+// 	),
+// array(
+
+// 	'ime'=>'Pero',
+// 	'prezime'=>'Perovski',
+// 	'telefon'=>'355001',
+
+// 	),
+// array(
+
+// 	'ime'=>'Janko',
+// 	'prezime'=>'Jankovski',
+// 	'telefon'=>'222110',
+
+// 	),
+// array(
+
+// 	'ime'=>'Stanko',
+// 	'prezime'=>'Stenkovski',
+// 	'telefon'=>'813214'
+
+// 	)
+
+
+// 	);
+
+// // print_r($glavna)
+
+// echo $tabela [3]['ime'];
+// echo '<br>';
+// echo $tabela [2]['prezime'];
+
+
+// Thirje te disa arrayve te nje pas njeshme.
+
+
+
+
+
+
+
+
+
+?>
